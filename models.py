3d9cef9cef76696f5edfc0f313d51dbf39b9db7e
Stephane Doiron <didymus1313@gmail.com>
2010-10-03 15:11:12 -0400

    address model passes basic tests

--- models.py
+++ models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Address(models.Model):
+    ADDRESS_TYPES = ((0, 'mailing'), (1, 'billing'), (2, 'shipping'),)
+
+    contact = models.ForeignKey(Contact)
+    addressType = models.PositiveIntegerField(choices=ADDRESS_TYPES, default=0)
+    street1 = models.CharField(max_length=255)
+    street2 = models.CharField(max_length=255, blank=True, null=True)
+    street3 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=128)
+    region = models.CharField(max_length=128)
+    country = models.CharField(max_length=128)
+    postalCode = models.CharField(max_length=16)
+    isDefault = models.BooleanField(default=False)
+
+    
--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models import *
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -20,4 +21,34 @@ Another way to test that 1 + 1 is equal to 2.
 >>> 1 + 1 == 2
 True
 """}
+
+
+class ContactTest (TestCase):
+    def testContactBasicInfo(self):
+        c = Contact(name='tester')
+        self.assertEquals('tester', c.name)
+
+class AddressTest (TestCase):
+    def testAddressBasicInfo(self):
+        c = Contact(name='tester')
+        a = Address(
+            contact = c,
+            addressType = 0,
+            street1 = '123 fake st',
+            street2 = 'po box 234',
+            street3 = 'station abc',
+            city = 'Testville',
+            region = 'testprovince',
+            country = 'canada',
+            postalCode = 'H0H0H0',
+        )
+        self.assertEquals('tester', a.contact.name)
+        self.assertEquals('123 fake st', a.street1)
+        self.assertEquals('po box 234', a.street2)
+        self.assertEquals('station abc', a.street3)
+        self.assertEquals('Testville', a.city)
+        self.assertEquals('testprovince', a.region)
+        self.assertEquals('canada', a.country)
+        self.assertEquals('H0H0H0', a.postalCode)
+        self.assertEquals('mailing', a.get_addressType_display())
 
