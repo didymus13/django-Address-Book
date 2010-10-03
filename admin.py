68934108682185d466ccf548bb68374eada57741
Stephane Doiron <didymus1313@gmail.com>
2010-10-03 19:03:46 -0400

    contact admin can manage related data

--- admin.py
+++ admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
 from models import *
 
-class ContactAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Contact, ContactAdmin)
-
 class AddressAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Address, AddressAdmin)
+
+class AddressInline(admin.TabularInline):
+    model = Address
 
 class TelephoneNumberAdmin(admin.ModelAdmin):
     pass
 admin.site.register(TelephoneNumber, TelephoneNumberAdmin)
 
+class TelephoneNumberInline(admin.TabularInline):
+    model = TelephoneNumber
+
 class EmailAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Email, EmailAdmin)
+
+class EmailInline(admin.TabularInline):
+    model = Email
+
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [
+        AddressInline,
+        TelephoneNumberInline,
+        EmailInline,
+    ]
+admin.site.register(Contact, ContactAdmin)
