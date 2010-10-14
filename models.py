9fcd8bc405859f505ec222713123714d1ef95f4b
Stephane Doiron <didymus1313@gmail.com>
2010-10-14 00:12:56 -0400

    added unicode methods

--- models.py
+++ models.py
@@ -4,6 +4,16 @@ from django.db import models
 class Contact(models.Model):
     firstname = models.CharField(max_length=128)
     lastname = models.CharField(max_length=128)
+    
+    def _makeFullName(self):
+        return "%s %s" % (self.firstname, self.lastname)
+    fullname = property(_makeFullName)
+    
+    def __unicode__(self):
+        return self.fullname
+    
+    def get_absolute_url(self):
+        return "/addressbook/api/contacts/%d" % (self.id)
 
 
 class Address(models.Model):
@@ -19,6 +29,12 @@ class Address(models.Model):
     country = models.CharField(max_length=128)
     postalCode = models.CharField(max_length=16)
     isDefault = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return self.street1
+
+    def get_absolute_url(self):
+        return "/addressbook/api/contacts/%d/addresses/%d" % (self.contact.id, self.id)
 
 class TelephoneNumber(models.Model):
     TELEPHONE_TYPE = ((0, 'telephone'), (1, 'fax'))
@@ -29,7 +45,19 @@ class TelephoneNumber(models.Model):
     numberType = models.PositiveIntegerField(choices=TELEPHONE_TYPE, default=0)
     isDefault = models.BooleanField(default=False)
 
+    def __unicode__(self):
+        return self.number
+
+    def get_absolute_url(self):
+        return "/addressbook/api/contacts/%d/telephone-numbers/%d/" % (self.contact.id, self.id)
+
 class Email(models.Model):
     contact = models.ForeignKey(Contact, related_name='emailAddresses')
     email = models.EmailField()
     isDefault = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.email
+
+    def get_absolute_url(self):
+        return "/addressbook/api/contacts/%d/emails/%d/" % (self.contact.id, self.id)
